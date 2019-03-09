915ff6f5870665f25a1f3e478b6ed736f89fa6e1
tomasz.no@op.pl <tomasz.no@op.pl>
2019-03-09 12:56:41 +0100

    polygon based labeler

--- main.py
+++ main.py
@@ -4,40 +4,23 @@ import os
 import numpy as np
 
 class GUI:
-    def __init__(self, path, images):
-        self.mouse_pressed = False
-        self.path = path
-        self.images = images
-        self.p0 = (0, 0)
-        self.p1 = (0, 0)
+    def __init__(self, vid_filename, output_directory):
+        self.vid_filename = vid_filename
+        self.output_directory = output_directory
+        self.frame_no = 0
         self.fname = None
         self.frame = None
-        self.mask = None
         self.overlay = None
-        self.rectSelect = True
-        self.label = 0
         self.alpha = 0.7
-        self.brush_size = 2
+        self.poly = []
+        self.free_mode = False
         cv2.namedWindow("Mask labeler", 0)
-        # cv2.setWindowProperty("Mask labeler", cv2.WINDOW_FULLSCREEN, True)
         cv2.setMouseCallback("Mask labeler", self.video_click)
         cv2.createTrackbar('Alpha', 'Mask labeler', 7, 10, self.update_alpha)
-        cv2.createTrackbar('Brush size', 'Mask labeler', 2, 10, self.update_brush)
-        switch = '0 : Background \n1 : Foreground'
-        cv2.createTrackbar(switch, 'Mask labeler', 0, 1, self.update_label)
-        try:
-            self.fname = self.path + next(self.images)
-            print(self.fname)
-            frame = cv2.imread(self.fname)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self.mask = np.full(frame.shape[:2], 0, np.uint8)
-            if frame is not None:
-                self.grabcut = Grabcut(frame, self.mask)
-                cv2.imshow("Mask labeler", frame)
-                self.frame = frame
-            print("Draw rectangle around object or press \"r\" to draw mask manually")
-        except StopIteration:
-            print("Invalid path or file")
+        if not os.path.exists(os.path.join(output_directory, 'images')):
+            os.makedirs(os.path.join(output_directory, 'images'))
+        if not os.path.exists(os.path.join(output_directory, 'labels')):
+            os.makedirs(os.path.join(output_directory, 'labels'))
 
     def update_alpha(self, x):
         self.alpha = x/10
@@ -45,137 +28,90 @@ class GUI:
                               1 - self.alpha, 0)
         cv2.imshow("Mask labeler", imm)
 
-    def update_brush(self, x):
-        self.brush_size = x
+    def run(self, img):
+        self.frame_no += 1
+        self.frame = img
+        if self.overlay is None:
+            cv2.imshow("Mask labeler", self.frame)
+        else:
+            imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
+                                  1 - self.alpha, 0)
+            cv2.imshow("Mask labeler", imm)
 
-    def update_label(self, x):
-        self.label = x
+    def save(self):
+        if self.overlay is not None:
+            label_mask = np.copy(self.overlay)
+            label_mask[label_mask == 255] = 1
+            label_fname = os.path.join(self.output_directory, "labels", self.vid_filename[:-4] +"_"+ str(self.frame_no) + "_label.jpg")
+            cv2.imwrite(label_fname, label_mask)
+            img_fname = os.path.join(self.output_directory, "images", self.vid_filename[:-4] +"_"+ str(self.frame_no) + ".jpg")
+            cv2.imwrite(img_fname, self.frame)
+            # self.poly = []
+            print("Saved", label_fname)
 
-    def run(self):
-        while True:
-            k = cv2.waitKey(1)
-            if k == ord('q'):
-                break
-            if k == ord('c'):
-                self.label = (self.label + 1) % 2
-                if self.label == 0:
-                    print("background")
-                if self.label == 1:
-                    print("foreground")
-            if k == ord('r'):
-                self.rectSelect = not self.rectSelect
-                if self.rectSelect:
-                    print("Select object to label")
-                else:
-                    self.overlay = np.full(self.frame.shape[:2], 0, np.uint8)
-                    print("Draw pixels manually")
-            if k == ord('t'):
-                if 1 in self.mask:
-                    print("Wait for mask")
-                    self.overlay = self.grabcut.refine_grabcut(self.mask)
-                    imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
-                                          1 - self.alpha, 0)
-                    cv2.imshow("Mask labeler", imm)
-                    print("Press \"n\" to save mask")
-                else:
-                    print("Select min one foreground point")
-            if k == ord('n'):
-                if self.overlay is not None:
-                    self.overlay[self.overlay == 255] = 1
-                    label_fname = self.fname[:-4] + "_label.jpg"
-                    cv2.imwrite(label_fname, self.overlay)
-                    print("Saved", label_fname)
-                self.overlay = None
-                self.rectSelect = True
-                print("Draw rectangle around object or press \"r\" to draw mask manually")
-                try:
-                    self.fname = args.image_directory + next(self.images)
-                    print(self.fname)
-                    frame = cv2.imread(self.fname)
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    if frame is not None:
-                        self.mask = np.full(frame.shape[:2], 0, np.uint8)
-                        self.frame = frame
-                        self.grabcut = Grabcut(frame, self.mask)
-                        cv2.imshow("Mask labeler", frame)
-                except StopIteration:
-                    break
 
     def video_click(self, e, x, y, flags, param):
-        if e == cv2.EVENT_MBUTTONDOWN:
-            self.mouse_pressed = True
-        if e == cv2.EVENT_MBUTTONUP:
-            self.mouse_pressed = False
         if e == cv2.EVENT_LBUTTONDOWN:
-            self.mouse_pressed = True
-            if not self.rectSelect:
-                self.mask[y:y+self.brush_size, x:x++self.brush_size] = self.label
-                if self.label == 0:
-                    self.overlay[y:y+self.brush_size, x:x+self.brush_size] = 0
-                if self.label == 1:
-                    self.overlay[y:y+self.brush_size, x:x+self.brush_size] = 255
+            self.poly.append([x, y])
+            if len(self.poly)>2:
+                self.overlay = np.full(self.frame.shape[:2], 0, np.uint8)
+                cv2.fillPoly(self.overlay, np.array(self.poly, dtype=np.int32)[np.newaxis, :, :],  255)
                 imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
                                       1 - self.alpha, 0)
                 cv2.imshow("Mask labeler", imm)
-            else:
-                self.p0 = (x, y)
-                print("rect start", x, y)
-        elif e == cv2.EVENT_LBUTTONUP:
-            self.mouse_pressed = False
-            if self.rectSelect:
-                self.p1 = (x, y)
-                rect = (self.p0[0], self.p0[1], abs(self.p1[0] - self.p0[0]), abs(self.p1[1] - self.p0[1]))
-                self.mask[self.p0[1]:self.p1[1], self.p0[0]:self.p1[0]] = 3
-                self.rectSelect = False
-                print("rect end", x, y)
-                print("Wait for mask")
-                try:
-                    self.overlay = self.grabcut.mask_rect(rect)
-                    imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
-                                          1 - self.alpha, 0)
-                    cv2.imshow("Mask labeler", imm)
-                    print("Mark wrong labeled areas")
-                    print("Press \"t\" to refine mask")
-                except cv2.error:
-                    self.rectSelect = True
-                    print("Select bigger rectangle")
-        elif e == cv2.EVENT_MOUSEMOVE:
-            if self.mouse_pressed and not self.rectSelect:
-                self.mask[y:y+self.brush_size, x:x++self.brush_size] = self.label
-                if self.label == 0:
-                    self.overlay[y:y+self.brush_size, x:x++self.brush_size] = 0
-                if self.label == 1:
-                    self.overlay[y:y+self.brush_size, x:x++self.brush_size] = 255
+        if e == cv2.EVENT_MBUTTONDOWN:
+            self.free_mode = False
+            self.overlay = np.full(self.frame.shape[:2], 0, np.uint8)
+            self.poly = []
+            imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
+                                  1 - self.alpha, 0)
+            cv2.imshow("Mask labeler", imm)
+
+        if e == cv2.EVENT_MOUSEMOVE:
+            if self.free_mode:
+                self.overlay = np.full(self.frame.shape[:2], 0, np.uint8)
+                xmin = np.max(np.array(self.poly)[:, 0])
+                ymin = np.max(np.array(self.poly)[:, 1])
+                relative_poly = np.stack((np.array(self.poly)[:, 0]-xmin, np.array(self.poly)[:, 1]-ymin), axis=1)
+                self.poly = np.stack((relative_poly[:, 0]+x, relative_poly[:,1]+y), axis=1)
+                cv2.fillPoly(self.overlay, np.array(self.poly, dtype=np.int32)[np.newaxis, :, :], 255)
                 imm = cv2.addWeighted(cv2.cvtColor(self.overlay, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
                                       1 - self.alpha, 0)
                 cv2.imshow("Mask labeler", imm)
-
-
-class Grabcut:
-    def __init__(self, frame, mask):
-        self.mask = mask
-        self.frame = frame
-        self.bgdModel = np.zeros((1, 65), np.float64)
-        self.fgdModel = np.zeros((1, 65), np.float64)
-
-    def mask_rect(self, rect):
-        self.mask = np.full(self.frame.shape[:2], 0, np.uint8)
-        cv2.grabCut(self.frame, self.mask, rect, self.bgdModel, self.fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-        mask2 = np.where((self.mask == 2) | (self.mask == 0), 0, 255).astype('uint8')
-        return mask2[:, :, np.newaxis]
-
-    def refine_grabcut(self, mask):
-        mask, bgdModel, fgdModel = cv2.grabCut(self.frame, mask, None, self.bgdModel, self.fgdModel, 5,
-                                               cv2.GC_INIT_WITH_MASK)
-        mask2 = np.where((mask == 2) | (mask == 0), 0, 255).astype('uint8')
-        return mask2[:, :, np.newaxis]
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-img', '--image_directory', dest='image_directory', type=str,
-                        default='/home/tnowak/Dataset/SBC/images/')
+    parser.add_argument('-vid', '--video-input', dest="video_input", type=str,
+                        default='/home/tnowak/Dataset/videos/12_05_03.avi')
+    parser.add_argument('-out', '--output_directory', dest='output_directory', type=str, default='/home/tnowak/DatasetNew/mask')
     args = parser.parse_args()
-    images = iter(os.listdir(args.image_directory))
-    gui = GUI(args.image_directory, images)
-    gui.run()
+    skip_frames = 2
+    print('Reading from video.', args.video_input)
+    vid_filename = args.video_input.split('/')[-1]
+    gui = GUI(vid_filename, args.output_directory)
+    video_capture = cv2.VideoCapture(args.video_input)
+    ret = True
+    frame = None
+    while ret:
+        k = cv2.waitKey(1)
+
+        if k == ord('q'):
+            break
+        if k == ord(' '):
+            gui.save()
+            gui.free_mode = True
+            video_capture.set(cv2.CAP_PROP_POS_FRAMES, video_capture.get(cv2.CAP_PROP_POS_FRAMES) + skip_frames)
+            ret, frame = video_capture.read()
+            if frame is not None:
+                gui.run(frame)
+        if k == ord('f'):
+            gui.free_mode = not gui.free_mode
+        if k == ord('b'):
+            skip_frames +=1
+            print(skip_frames)
+        if k == ord('n'):
+            skip_frames-=1
+            print(skip_frames)
+
+
+    video_capture.stop()
