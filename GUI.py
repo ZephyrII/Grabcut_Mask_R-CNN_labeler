2bd0e66c2cf463abc18c030bdcf8fd6e72fcba5c
ZephyrII <tomasz.no@op.pl>
2019-05-10 08:21:27 +0200

    mat file metadata

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import cv2
+
 try:
     from cv2 import cv2
 except ImportError:
@@ -6,12 +7,11 @@ except ImportError:
 import numpy as np
 from Detector import Detector
 import os
-import xml.etree.ElementTree as ET
+import scipy.io
+import apriltag
 
-camera_matrix = np.array([[1929.14559, 0, 1924.38974],
-                          [0, 1924.07499, 1100.54838],
-                          [0, 0, 1]])
-camera_distortion = (-0.25591, 0.07370, 0.00017, -0.00002)
+camera_distortion = (-0.0301, 0.03641, 0.001298, -0.00111)
+
 
 class GUI:
     def __init__(self, path_to_model, output_directory, vid_filename, video_capture):
@@ -19,7 +19,6 @@ class GUI:
         self.output_directory = output_directory
         self.mouse_pressed = False
         self.mask = None
-        self.kp = []
         self.overlay = None
         self.frame = None
         self.init_offset = None
@@ -27,10 +26,12 @@ class GUI:
         self.alpha = 0.4
         self.brush_size = 4
         self.video_capture = video_capture
-        self.video_capture.set(cv2.CAP_PROP_POS_MSEC, 200*1000)
+        # self.video_capture.set(cv2.CAP_PROP_POS_MSEC, 200 * 1000)
         self.path_to_model = path_to_model
         self.frame_no = self.video_capture.get(cv2.CAP_PROP_POS_FRAMES)
-        self.grabcut = Grabcut()
+        self.camera_matrix = np.array([[1929.14559, 0, 1924.38974],
+                                      [0, 1924.07499, 1100.54838],
+                                      [0, 0, 1]])
 
         cv2.namedWindow("Mask labeler", 0)
         cv2.setMouseCallback("Mask labeler", self.video_click)
@@ -43,9 +44,8 @@ class GUI:
         if not os.path.exists(os.path.join(output_directory, 'annotations')):
             os.makedirs(os.path.join(output_directory, 'annotations'))
 
-
     def update_alpha(self, x):
-        self.alpha = x/10
+        self.alpha = x / 10
         self.overlay = self.mask
         self.overlay = np.where(self.overlay == cv2.GC_PR_BGD, 0, self.overlay)
         imm = cv2.addWeighted(cv2.cvtColor(self.overlay * 255, cv2.COLOR_GRAY2BGR), self.alpha, self.frame,
@@ -57,14 +57,14 @@ class GUI:
 
     def run_video(self):
         ret, frame = self.video_capture.read()
-        detector = Detector(frame, self.path_to_model, camera_matrix)
         while True:
             self.frame_no = self.video_capture.get(cv2.CAP_PROP_POS_FRAMES)
             if frame is not None:
+                detector = Detector(frame, self.path_to_model, self.camera_matrix)
                 # img_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
                 # img_yuv[:, :, 2] = cv2.equalizeHist(img_yuv[:, :, 2])
                 # frame = cv2.cvtColor(img_yuv, cv2.COLOR_HSV2BGR)
-                frame = cv2.undistort(frame, camera_matrix, camera_distortion)
+                frame = cv2.undistort(frame, self.camera_matrix, camera_distortion)
                 cv2.imshow('Mask labeler', frame)
                 # if self.init_offset is None:
                 #     cv2.waitKey(0)
@@ -79,9 +79,9 @@ class GUI:
             else:
                 # frame_no = self.video_capture.get(cv2.CAP_PROP_POS_FRAMES)
                 last_frame_no = self.frame_no
-                cnt=1
-                while self.frame_no-last_frame_no==0:
-                    self.video_capture.set(cv2.CAP_PROP_POS_FRAMES,  self.frame_no + cnt)
+                cnt = 1
+                while self.frame_no - last_frame_no == 0:
+                    self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, self.frame_no + cnt)
                     last_frame_no = self.video_capture.get(cv2.CAP_PROP_POS_FRAMES)
                     cnt += 1
                     print(self.frame_no, last_frame_no, cnt)
@@ -101,24 +101,21 @@ class GUI:
                 self.show_mask()
                 k = cv2.waitKey(0)
             if k == ord('s'):
-                self.video_capture.set(cv2.CAP_PROP_POS_FRAMES,  self.frame_no + 3)
+                self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, self.frame_no + 3)
             if k == ord(' '):
-                if len(self.kp) != 6:
-                    print("SELECT KEYPOINTS!", len(self.kp))
-                    self.show_warning_window("SELECT KEYPOINTS!")
-                    self.kp = []
-                    continue
                 self.save()
-                self.video_capture.set(cv2.CAP_PROP_POS_FRAMES,  self.frame_no + 3)
+                self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, self.frame_no + 3)
                 ma_alpha = 0.9
                 overlay = np.zeros(frame.shape[:2], dtype=np.uint8)
-                overlay[detector.offset[0]:detector.offset[0] + detector.slice_size[0], detector.offset[1]:detector.offset[1] + detector.slice_size[1]] = self.overlay*100
-                detector.moving_avg_image = cv2.addWeighted(detector.moving_avg_image, ma_alpha, overlay.astype(np.uint8), 1 - ma_alpha, 0, detector.moving_avg_image)
+                overlay[detector.offset[0]:detector.offset[0] + detector.slice_size[0],
+                detector.offset[1]:detector.offset[1] + detector.slice_size[1]] = self.overlay * 100
+                detector.moving_avg_image = cv2.addWeighted(detector.moving_avg_image, ma_alpha,
+                                                            overlay.astype(np.uint8), 1 - ma_alpha, 0,
+                                                            detector.moving_avg_image)
                 # cv2.imshow("lol", detector.moving_avg_image)
             ret, frame = self.video_capture.read()
 
         self.video_capture.release()
-
 
     def show_warning_window(self, message):
         img = np.zeros((200, 600, 3))
@@ -132,100 +129,70 @@ class GUI:
         cv2.imshow("Mask labeler", imm)
 
     def video_click(self, e, x, y, flags, param):
-            if e == cv2.EVENT_MBUTTONDOWN:
-                self.mouse_pressed = True
-                self.label = 0
-                cv2.circle(self.mask, (x, y), self.brush_size,  self.label, thickness=-1)
-            if e == cv2.EVENT_MBUTTONUP:
-                self.mouse_pressed = False
-            if e == cv2.EVENT_RBUTTONDOWN:
-                self.kp.append((x,y))
-            if e == cv2.EVENT_LBUTTONDOWN:
-                # if self.init_offset is not None:
-                self.mouse_pressed = True
-                self.label = 1
-                # self.mask[y-self.brush_size:y + self.brush_size, x-self.brush_size:x +self.brush_size] = self.label+2
-                cv2.circle(self.mask, (x, y), self.brush_size,  self.label, thickness=-1)
-                # self.mask[y:y + self.brush_size, x:x + +self.brush_size] = self.label
-                self.show_mask()
-            elif e == cv2.EVENT_LBUTTONUP:
-                self.mouse_pressed = False
+        # if e == cv2.EVENT_MBUTTONDOWN:
+        if e == cv2.EVENT_RBUTTONUP:
+            self.mouse_pressed = False
+        if e == cv2.EVENT_RBUTTONDOWN:
+            self.mouse_pressed = True
+            self.label = 0
+            cv2.circle(self.mask, (x, y), self.brush_size, self.label, thickness=-1)
+        if e == cv2.EVENT_LBUTTONDOWN:
+            self.mouse_pressed = True
+            self.label = 1
+            cv2.circle(self.mask, (x, y), self.brush_size, self.label, thickness=-1)
+            self.show_mask()
+        elif e == cv2.EVENT_LBUTTONUP:
+            self.mouse_pressed = False
 
-            elif e == cv2.EVENT_MOUSEMOVE:
-                if self.mouse_pressed:
-                    # self.mask[y-self.brush_size:y + self.brush_size, x-self.brush_size:x +self.brush_size] = self.label+2
-                    cv2.circle(self.mask, (x, y), self.brush_size, self.label, thickness=-1)
-                    # self.mask[y:y + self.brush_size, x:x +self.brush_size] = self.label
-                    self.show_mask()
+        elif e == cv2.EVENT_MOUSEMOVE:
+            if self.mouse_pressed:
+                cv2.circle(self.mask, (x, y), self.brush_size, self.label, thickness=-1)
+                self.show_mask()
+
+    def apriltag_pose(self, img):
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        options = apriltag.DetectorOptions(families='tag36h11',
+                                           border=1,
+                                           nthreads=4,
+                                           quad_decimate=1.0,
+                                           quad_blur=0.0,
+                                           refine_edges=True,
+                                           refine_decode=True,
+                                           refine_pose=True,
+                                           debug=True,
+                                           quad_contours=False)
+        detector = apriltag.Detector(options)
+        result = detector.detect(img_gray)
+        if len(result) > 0:
+            pose, e0, e1 = detector.detection_pose(result[0], (
+                self.camera_matrix[0, 0], self.camera_matrix[1, 1], self.camera_matrix[0, 2], self.camera_matrix[1, 2]),
+                                                   1.0)
+            return pose
+        else:
+            return None
 
     def save(self):
         if self.mask is not None:
             label_mask = np.copy(self.mask)
             mask_coords = np.argwhere(label_mask == 1)
+            center = (np.mean(mask_coords[:, 0]), np.mean(mask_coords[:, 1]))
 
-            # label_mask[label_mask > cv2.GC_FGD] = 0
-            label_fname = os.path.join(self.output_directory, "labels", self.vid_filename[:-4] +"_"+ str(int(self.frame_no)) + "_label.jpg")
+            label_fname = os.path.join(self.output_directory, "labels",
+                                       self.vid_filename[:-4] + "_" + str(int(self.frame_no)) + "_label.jpg")
             cv2.imwrite(label_fname, label_mask)
-            img_fname = os.path.join(self.output_directory, "images", self.vid_filename[:-4] +"_"+ str(int(self.frame_no)) + ".jpg")
+            img_fname = os.path.join(self.output_directory, "images",
+                                     self.vid_filename[:-4] + "_" + str(int(self.frame_no)) + ".jpg")
             cv2.imwrite(img_fname, self.frame)
-            ann_fname = os.path.join(self.output_directory, "annotations", self.vid_filename[:-4] +"_"+ str(int(self.frame_no)) + ".txt")
-            with open(ann_fname, 'w') as f:
-                f.write(self.makeXml(mask_coords, self.kp, "charger", self.frame.shape[1], self.frame.shape[0], ann_fname))
-            self.kp = []
-            # self.poly = []
-            print("Saved", label_fname)
+            ann_fname = os.path.join(self.output_directory, "annotations",
+                                     self.vid_filename[:-4] + "_" + str(int(self.frame_no)) + ".mat")
+            pose = self.apriltag_pose(self.frame)
+            if pose is not None:
+                self.save_metadata(center, pose, ann_fname)
+                print("Saved", label_fname)
+            else:
+                print("AprilTag pose is None")
 
-    def makeXml(self, mask_coords, keypoints_list,  className, imgWidth, imgHeigth, filename):
-        rel_xmin = np.min(mask_coords[:, 1])
-        rel_ymin = np.min(mask_coords[:, 0])
-        rel_xmax = np.max(mask_coords[:, 1])
-        rel_ymax = np.max(mask_coords[:, 0])
-        xmin = rel_xmin / imgWidth
-        ymin = rel_ymin / imgHeigth
-        xmax = rel_xmax / imgWidth
-        ymax = rel_ymax / imgHeigth
-        ann = ET.Element('annotation')
-        ET.SubElement(ann, 'folder').text = 'images'
-        ET.SubElement(ann, 'filename').text = filename + ".jpg"
-        ET.SubElement(ann, 'path')
-        source = ET.SubElement(ann, 'source')
-        ET.SubElement(source, 'database').text = "Unknown"
-        size = ET.SubElement(ann, 'size')
-        ET.SubElement(size, 'width').text = str(imgWidth)
-        ET.SubElement(size, 'height').text = str(imgHeigth)
-        ET.SubElement(size, 'depth').text = "3"
-        ET.SubElement(ann, 'segmented').text = "0"
-        object = ET.SubElement(ann, 'object')
-        ET.SubElement(object, 'name').text = className
-        ET.SubElement(object, 'pose').text = "Unspecified"
-        ET.SubElement(object, 'truncated').text = "0"
-        ET.SubElement(object, 'difficult').text = "0"
-        bndbox = ET.SubElement(object, 'bndbox')
-        ET.SubElement(bndbox, 'xmin').text = str(xmin)
-        ET.SubElement(bndbox, 'ymin').text = str(ymin)
-        ET.SubElement(bndbox, 'xmax').text = str(xmax)
-        ET.SubElement(bndbox, 'ymax').text = str(ymax)
-        keypoints = ET.SubElement(object, 'keypoints')
-        for i, kp in enumerate(keypoints_list):
-            xml_kp = ET.SubElement(keypoints, 'keypoint'+str(i))
-            ET.SubElement(xml_kp, 'x').text = str(kp[0])
-            ET.SubElement(xml_kp, 'y').text = str(kp[1])
-        return ET.tostring(ann, encoding='unicode', method='xml')
-
-class Grabcut:
-    def __init__(self):
-        self.bgdModel = np.zeros((1, 65), np.float64)
-        self.fgdModel = np.zeros((1, 65), np.float64)
-
-    # def mask_rect(self, rect):
-    #     self.mask = np.full(self.frame.shape[:2], 0, np.uint8)
-    #     cv2.grabCut(self.frame, self.mask, rect, self.bgdModel, self.fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-    #     mask2 = np.where((self.mask == 2) | (self.mask == 0), 0, 255).astype('uint8')
-    #     return mask2[:, :, np.newaxis]
-
-    def refine_grabcut(self, frame, mask):
-        mask, bgdModel, fgdModel = cv2.grabCut(frame, mask, None, self.bgdModel, self.fgdModel, 15,
-                                               cv2.GC_INIT_WITH_MASK)
-        # mask2 = np.where((mask == 2) | (mask == 0), 0, 255).astype('uint8')
-        return mask
-
+    def save_metadata(self, center, pose, filename):
+        results = {'center': np.expand_dims(center, -1), 'cls_indexes': [1], 'factor_depth': 1, 'intrinsics_matrix': self.camera_matrix,
+                   'poses': np.expand_dims(pose[:3], -1), 'rotation_translation_matrix': np.zeros((3, 4)), 'vertmap': np.zeros((0, 0, 3))}
+        scipy.io.savemat(filename, results, do_compression=True)
--- labeler.py
+++ labeler.py
@@ -1,9 +1,4 @@
 import cv2
-# from cv2 import cv2
-# try:
-#     pass
-# except ImportError:
-#     pass
 import os
 import argparse
 from GUI import GUI
@@ -11,13 +6,13 @@ from GUI import GUI
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-vid', '--video-input', dest="video_input", type=str,
-                        default='/root/share/tf/videos/GH010350.MP4')
+                        default='/root/share/videos/GH010324.MP4')
     parser.add_argument('-img', '--image_directory', dest='image_directory', type=str)
     parser.add_argument('-out', '--output_directory', dest='output_directory', type=str)
     args = parser.parse_args()
     vid_filename = args.video_input.split('/')[-1]
 
-    path_to_model = os.path.join("/root/share/tf/Mask/model/8.04/", 'frozen_inference_graph.pb')
+    path_to_model = os.path.join("/root/share/Mask/model/18.03/", 'frozen_inference_graph.pb')
     video_capture = cv2.VideoCapture(args.video_input)
     gui = GUI(path_to_model, args.output_directory, vid_filename, video_capture)
     gui.run_video()
