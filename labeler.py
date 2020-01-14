c935e4b387b49c25411382f092229895f52b5178
ZephyrII <tomasz.no@op.pl>
2020-01-14 15:21:49 +0100

    yaw angle

--- labeler.py
+++ labeler.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import signal
 import numpy as np
 from numpy import linalg
 import math
@@ -26,23 +27,24 @@ class DetectorNode:
     def __init__(self):
         self.camera_topic = '/blackfly/camera/image_color/compressed'
         # self.camera_topic = '/video_player/compressed'
-        self.gt_pose_topic = '/pose_estimator/charger_pose/gt'
+        self.gt_pose_topic = '/pose_estimator/tomek'
         self.imu_topic = '/xsens/data'
-        self.output_directory = '/root/share/tf/dataset/semi-sup'
-        rospy.init_node('deep_pose_estimator')
+        self.output_directory = '/root/share/tf/dataset/Inea/yaw'
+        rospy.init_node('labeler')
         self.scale_factor = 1.0
-        path_to_charger_model = os.path.join("/root/share/tf/Mask/model/27_08_self-train", 'frozen_inference_graph.pb')
+        # path_to_charger_model = os.path.join("/root/share/tf/Mask/model/27_08_self-train", 'frozen_inference_graph.pb')
         # path_to_charger_model = os.path.join("/root/share/tf/Mask/model/11_09_st", 'frozen_inference_graph.pb')
-        path_to_pole_model = os.path.join("/root/share/tf/Faster/pole/model_A8", 'frozen_inference_graph.pb')
-        command = "rosbag play --skip-empty=5 -r 1 /root/share/tf/dataset/warsaw/rosbags/27-14-04-47_7.bag "
-        self.detector = Detector(path_to_charger_model, path_to_pole_model)
+        # path_to_pole_model = os.path.join("/root/share/tf/Faster/pole/model_A8", 'frozen_inference_graph.pb')
+        command = "rosbag play -r 1 -s 142 /root/share/tf/dataset/Inea/interpolated/_2019-11-20-10-09-22.bag"
+        # self.detector = Detector(path_to_charger_model, path_to_pole_model)
         self.p = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True)
         self.frame_shape = self.get_image_shape()
         self.frame_shape = [int(self.frame_shape[0] * self.scale_factor), int(self.frame_shape[1] * self.scale_factor)]
-        self.detector.init_size(self.frame_shape)
+        # self.detector.init_size(self.frame_shape)
         self.slice_size = (720, 960)
-        self.mask = None
-        self.keypoints = None
+        self.mask = np.zeros(self.frame_shape[:2], dtype=np.uint8)
+        # self.overlay = np.zeros(self.frame_shape[:2], dtype=np.uint8)
+        self.keypoints = []
         self.poly = []
         self.image_msg = None
         self.image = None
@@ -52,8 +54,8 @@ class DetectorNode:
         self.gt_pose = None
         self.frame_gt = None
         self.frame_time = None
-        self.camera_matrix = np.array([[5008.72 * self.scale_factor, 0, 2771.21 * self.scale_factor],
-                                       [0, 5018.43 * self.scale_factor, 1722.90 * self.scale_factor],
+        self.camera_matrix = np.array([[4996.73451 * self.scale_factor, 0, 2732.95188 * self.scale_factor],
+                                       [0, 4992.93867 * self.scale_factor, 1890.88113 * self.scale_factor],
                                        [0, 0, 1]])
         if not os.path.exists(os.path.join(self.output_directory, "mask", 'images')):
             os.makedirs(os.path.join(self.output_directory, "mask", 'images'))
@@ -84,51 +86,42 @@ class DetectorNode:
         rospy.Subscriber(self.imu_topic, Imu, self.get_imu_transform, queue_size=1)
         while not rospy.is_shutdown():
             if self.image is not None:
-                self.toggle_rosbag_play()
-                self.detect(self.image)
-                if self.detector.best_detection is not None:
-                    if int(self.detector.best_detection['score'][0]) > 98:
-                        k = ord('n')
-                    else:
-                        k = cv2.waitKey(0)
-                else:
-                    k = 27
-                if k == 27:
-                    self.keypoints = []
-                    self.poly = []
-                    self.mask = np.zeros(self.image.shape[:2], np.uint8)
-                    self.show_mask()
-                    k = cv2.waitKey(0)
-                    if k == ord('s'):
-                        self.toggle_rosbag_play()
-                    # if k == ord('r'):
-                    #     self.keypoints = []
-                    #     self.poly = []
-                    #     self.show_mask()
-                    if k == ord('q'):
-                        exit(0)
-                    if k == ord('n'):
-                        self.save_all(100)
-                        self.toggle_rosbag_play()
-                        continue
+                self.show_mask()
+                k = cv2.waitKey(0)
                 if k == ord('q'):
-                    self.p.kill()
+                    self.p.send_signal(signal.SIGINT)
                     exit(0)
-                if k == ord('n'):
-                    self.save_all()
+                if k == ord('s'):
+                    self.image = None
                     self.toggle_rosbag_play()
+                if k == ord('r'):
+                    self.poly = []
+                    self.keypoints = []
+                    self.mask = np.zeros(self.frame_shape[:2], dtype=np.uint8)
+                    self.show_mask()
+                if k == ord('n'):
+                    if len(self.keypoints) != 6:
+                        print("SELECT KEYPOINTS!", len(self.keypoints))
+                        self.keypoints = []
+                        cv2.waitKey(0)
+                    self.save_all(100)
         rospy.spin()
 
     def save_all(self, score=None):
-        if score is None:
-            score = self.detector.best_detection['score'][0]
-        if self.frame_gt is not None:
-            self.save_mask(self.frame_time, self.mask, score, self.frame_gt)
-            self.save_poseCNN(self.frame_time, self.mask, self.frame_gt)
-        else:
-            self.save_mask(self.frame_time, self.mask, score)
-        self.detector.best_detection = None
+        # if score is None:
+        #     score = self.detector.best_detection['score'][0]
+        print(self.frame_gt)
+        self.save_mask(self.frame_time, self.mask, score, self.frame_gt)
+        # if self.frame_gt is not None:
+        #     self.save_mask(self.frame_time, self.mask, score, self.frame_gt)
+        #     self.save_poseCNN(self.frame_time, self.mask, self.frame_gt)
+        # else:
+        #     self.save_mask(self.frame_time, self.mask, score)
+        # self.detector.best_detection = None
         self.image = None
+        self.mask = np.zeros(self.frame_shape[:2], dtype=np.uint8)
+        # self.overlay = np.zeros(self.frame_shape[:2], dtype=np.uint8)
+        self.toggle_rosbag_play()
 
     def toggle_rosbag_play(self):
         # self.keyboard.press(Key.space)
@@ -146,22 +139,21 @@ class DetectorNode:
 
     def update_gt(self, gt_msg):
         self.gt_pose = gt_msg
-        self.gt_mat = np.identity(4)
-        self.gt_mat[0, 3] = self.gt_pose.pose.position.x
-        self.gt_mat[1, 3] = self.gt_pose.pose.position.y
-        self.gt_mat[2, 3] = self.gt_pose.pose.position.z
-        if self.r_mat is not None:
-            self.gt_pose_imu = np.matmul(self.r_mat, self.gt_mat)
+        # self.gt_mat = np.identity(4)
+        # self.gt_mat[0, 3] = self.gt_pose.pose.position.x
+        # self.gt_mat[1, 3] = self.gt_pose.pose.position.y
+        # self.gt_mat[2, 3] = self.gt_pose.pose.position.z
+        # if self.r_mat is not None:
+        #     self.gt_pose_imu = np.matmul(self.r_mat, self.gt_mat)
 
     def update_image(self, image_msg):
+        self.toggle_rosbag_play()
         self.image_msg = image_msg
-        np_arr = np.fromstring(image_msg.data, np.uint8)
+        np_arr = np.fromstring(self.image_msg.data, np.uint8)
         image = cv2.imdecode(np_arr, -1)
-        self.image = cv2.resize(image, None, fx=self.scale_factor, fy=self.scale_factor)
-        # cv2.imshow('deteion', cv2.resize(self.image, (1280, 960)))
-        # cv2.waitKey(10)
         self.frame_time = self.image_msg.header.stamp
-        self.frame_gt = self.gt_pose_imu
+        self.frame_gt = self.gt_pose
+        self.image = cv2.resize(image, None, fx=self.scale_factor, fy=self.scale_factor)
 
         # img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         # img_yuv[:, :, 2] = cv2.equalizeHist(img_yuv[:, :, 2])
@@ -169,29 +161,28 @@ class DetectorNode:
         # self.image = cv2.resize(image, (self.frame_shape[1], self.frame_shape[0]))
         # cv2.imshow('look', cv2.resize(image, (1280, 960)))
 
-    def detect(self, frame):
-        working_copy = np.copy(frame)
-        disp = np.copy(frame)
-        self.detector.detect(working_copy)
-        if self.detector.best_detection is not None:
-            print("GT:", self.gt_mat)
-            print("IMU:", self.gt_pose_imu)
-            print("Charger score:", self.detector.best_detection['score'][0])
-            for idx, pt in enumerate(self.detector.best_detection['keypoints']):
-                cv2.circle(disp, (int(pt[0]), int(pt[1])), 5, (0, 255, 0), -1)
-
-            self.keypoints = self.detector.best_detection['keypoints']
-            x1, y1, x2, y2 = self.detector.best_detection['abs_rect']
-            self.mask = np.zeros(self.image.shape[:2], np.uint8)
-            self.mask[y1:y2, x1:x2] = np.where(self.detector.mask_reshaped > 0.5, 1, 0) #, cv2.COLOR_GRAY2BGR)
-            imgray = self.detector.mask_reshaped*255
-            ret, thresh = cv2.threshold(imgray.astype(np.uint8), 127, 255, 0)
-            im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            if len(contours)>0:
-                contour = np.add(contours[0], [x1, y1])
-                cv2.drawContours(disp, [contour], 0, (255, 250, 250), 2)
-            cv2.imshow('Mask labeler', cv2.resize(disp, (1280, 960)))
-
+    # def detect(self, frame):
+    #     working_copy = np.copy(frame)
+    #     disp = np.copy(frame)
+    #     self.detector.detect(working_copy)
+    #     if self.detector.best_detection is not None:
+    #         print("GT:", self.gt_mat)
+    #         print("IMU:", self.gt_pose_imu)
+    #         print("Charger score:", self.detector.best_detection['score'][0])
+    #         for idx, pt in enumerate(self.detector.best_detection['keypoints']):
+    #             cv2.circle(disp, (int(pt[0]), int(pt[1])), 5, (0, 255, 0), -1)
+    #
+    #         self.keypoints = self.detector.best_detection['keypoints']
+    #         x1, y1, x2, y2 = self.detector.best_detection['abs_rect']
+    #         self.mask = np.zeros(self.image.shape[:2], np.uint8)
+    #         self.mask[y1:y2, x1:x2] = np.where(self.detector.mask_reshaped > 0.5, 1, 0) #, cv2.COLOR_GRAY2BGR)
+    #         imgray = self.detector.mask_reshaped*255
+    #         ret, thresh = cv2.threshold(imgray.astype(np.uint8), 127, 255, 0)
+    #         im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #         if len(contours)>0:
+    #             contour = np.add(contours[0], [x1, y1])
+    #             cv2.drawContours(disp, [contour], 0, (255, 250, 250), 2)
+    #         cv2.imshow('Mask labeler', cv2.resize(disp, (1280, 960)))
 
     def show_mask(self):
         alpha = 0.1
@@ -218,12 +209,11 @@ class DetectorNode:
                 resized_label = cv2.resize(label_mask, None, fx=res, fy=res)
                 resized_image = cv2.resize(image, None, fx=res, fy=res)
 
-                scaled_kp = (np.array(self.keypoints) / np.array(self.image.shape[:2])) * np.array(
-                    resized_image.shape[:2])
+                scaled_kp = (np.array(self.keypoints) / np.array(self.image.shape[:2])) * np.array(resized_image.shape[:2])
                 crop_offset = scaled_kp[0] - tuple(x / 2 for x in self.slice_size)
                 crop_offset = [int(max(min(crop_offset[0], resized_image.shape[1] - self.slice_size[0]), 0)),
                                int(max(min(crop_offset[1], resized_image.shape[0] - self.slice_size[1]), 0))]
-                final_kp = scaled_kp - crop_offset
+                final_kp = (scaled_kp - crop_offset) / np.array(self.slice_size)
                 final_label = resized_label[crop_offset[1]:crop_offset[1] + self.slice_size[0],
                               crop_offset[0]:crop_offset[0] + self.slice_size[1]]
                 final_image = resized_image[crop_offset[1]:crop_offset[1] + self.slice_size[0],
@@ -249,17 +239,20 @@ class DetectorNode:
                 ann_fname = os.path.join(self.output_directory, "mask", "annotations",
                                          str(res) + '_' + str(stamp) + ".txt")
                 if gt_pose is not None:
-                    distance = math.sqrt((pow(gt_pose[0, 3], 2)+pow(gt_pose[2, 3], 2)))
+                    distance = math.sqrt((pow(gt_pose.pose.position.x, 2)+pow(gt_pose.pose.position.z, 2)))
+                    theta = np.arcsin(-2 * (gt_pose.pose.orientation.x * gt_pose.pose.orientation.z -
+                                            gt_pose.pose.orientation.w * gt_pose.pose.orientation.y))
                 else:
                     distance = 0
+                    theta = 0
 
                 with open(ann_fname, 'w') as f:
                     f.write(self.makeXml(mask_coords, final_kp, "charger", final_image.shape[1], final_image.shape[0],
-                                         ann_fname, distance, score, self.detector.offset))
+                                         ann_fname, distance, score, crop_offset, theta))
 
             print("Saved", label_fname)
 
-    def makeXml(self, mask_coords, keypoints_list, className, imgWidth, imgHeigth, filename, distance, score, offset):
+    def makeXml(self, mask_coords, keypoints_list, className, imgWidth, imgHeigth, filename, distance, score, offset, theta): # TODO:
         rel_xmin = np.min(mask_coords[:, 1])
         rel_ymin = np.min(mask_coords[:, 0])
         rel_xmax = np.max(mask_coords[:, 1])
@@ -279,8 +272,8 @@ class DetectorNode:
         ET.SubElement(size, 'height').text = str(imgHeigth)
         ET.SubElement(size, 'depth').text = "3"
         ET.SubElement(ann, 'segmented').text = "0"
-        ET.SubElement(ann, 'offset_x').text = str(offset[1])
-        ET.SubElement(ann, 'offset_y').text = str(offset[0])
+        ET.SubElement(ann, 'offset_x').text = str(offset[0])
+        ET.SubElement(ann, 'offset_y').text = str(offset[1])
         object = ET.SubElement(ann, 'object')
         ET.SubElement(object, 'name').text = className
         ET.SubElement(object, 'pose').text = "Unspecified"
@@ -292,12 +285,13 @@ class DetectorNode:
         ET.SubElement(bndbox, 'xmax').text = str(xmax)
         ET.SubElement(bndbox, 'ymax').text = str(ymax)
         ET.SubElement(object, 'distance').text = str(distance)
+        ET.SubElement(object, 'theta').text = str(theta)
         ET.SubElement(object, 'weight').text = str(score)
         keypoints = ET.SubElement(object, 'keypoints')
         for i, kp in enumerate(keypoints_list):
             xml_kp = ET.SubElement(keypoints, 'keypoint' + str(i))
-            ET.SubElement(xml_kp, 'x').text = str(int(kp[0]))
-            ET.SubElement(xml_kp, 'y').text = str(int(kp[1]))
+            ET.SubElement(xml_kp, 'x').text = str(kp[0])
+            ET.SubElement(xml_kp, 'y').text = str(kp[1])
         return ET.tostring(ann, encoding='unicode', method='xml')
 
     def save_poseCNN(self, stamp, mask, gt_pose):
